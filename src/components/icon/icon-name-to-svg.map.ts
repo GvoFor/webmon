9d2b9d49c170f3b2b1e607a07b5bb30d04adc311
Pumnia Mykhailo <misha.pumnya@gmail.com>
2025-05-01 19:08:47 +0300

    feat: add icon component wm-2

---
+++ src/components/icon/icon-name-to-svg.map.ts
@@ -0,0 +1,13 @@
+import { IconName } from './types';
+import GithubIcon from '/src/assets/images/icons/github.svg?react';
+import LinkedinIcon from '/src/assets/images/icons/linkedin.svg?react';
+
+const iconNameToSvgMap: Record<
+  IconName,
+  React.FC<React.SVGProps<SVGSVGElement>>
+> = {
+  'github': GithubIcon,
+  'linkedin': LinkedinIcon,
+};
+
+export { iconNameToSvgMap };
