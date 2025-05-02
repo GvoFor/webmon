fa2ee0cea9b0b97b5f6729df69d8a2c8eb7d3544
Pumnia Mykhailo <misha.pumnya@gmail.com>
2025-05-02 12:43:57 +0300

    feat: add home page wm-21

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,11 +1,28 @@
-type Properties = {};
-
 import styles from './styles.module.scss';
 
-const Home = ({}: Properties): React.JSX.Element => {
+const Home = (): React.JSX.Element => {
   return (
     <>
-      <h1 className={styles['welcome']}>Welcome Home</h1>
+      <section className={styles['intro-section']}>
+        <p>
+          Keep track your favourite video-content with{' '}
+          <span className={styles['accent']}>Webmon</span> easily.
+        </p>
+        <ol className={styles['steps']}>
+          <li className={styles['step']}>
+            <p>
+              Create and setup <span className={styles['accent']}>script</span>
+            </p>
+          </li>
+          <li className={styles['step']}>
+            <p>Be reported if new content available </p>
+          </li>
+        </ol>
+        <div className={styles['showcase-container']}>
+          {/* TODO: replace with real showcase-video */}
+          <div className={styles['showcase-video']}>Showcase</div>
+        </div>
+      </section>
     </>
   );
 };
