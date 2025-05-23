07d1328b7da0f5f951b16d50498f019046ae0f11
Pumnia Mykhailo <misha.pumnya@gmail.com>
2025-05-23 14:30:28 +0300

    feat: setup basic express js server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,18 @@
-console.log('Hello from backend');
+import express from 'express';
+import cors from 'cors';
+import { config } from './modules/modules.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({ origin: '*' }));
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello from backend</h1>');
+});
+
+app.listen(config.APP_PORT, config.APP_HOST, () => {
+  console.log(
+    `Backend listening on http://${config.APP_HOST}:${config.APP_PORT}`,
+  );
+});
